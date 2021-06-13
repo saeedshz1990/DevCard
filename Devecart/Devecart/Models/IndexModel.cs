edf920a1efffab6318505643f24001bcbdf02fd3
Saeed Ansari <saeed.ansari1990@hotmail.com>
2021-06-13 05:06:41 +0430

    Project Section From index And Create Partial View

---
+++ Devecart/Devecart/Models/IndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Devecart.Models
+{
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+
+    }
+}
