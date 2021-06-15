c4b5bc9c112c55e0d281dd27d0cc23e5b4c80cad
Saeed Ansari <saeed.ansari1990@hotmail.com>
2021-06-15 05:23:13 +0430

    LatestArticles View component created

--- Devecart/Devecart/Models/Article.cs
+++ Devecart/Devecart/Models/Article.cs
@@ -8,5 +8,14 @@ namespace Devecart.Models
         public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
+
+        public Article(long id, string title, string description, string image)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+        }
     }
 }
---
+++ Devecart/Devecart/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Devecart.Models;
+using System.Collections.Generic;
+
+namespace Devecart.ViewComponents
+{
+    public class LatestArticlesViewComponent: ViewComponent
+    {
+public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(id:1,title:"Learn Asp.net core mvc",description:" Best Of seller",image:"blog-post-thumb-1.jpg"),
+                new Article(id:2,title:"Learn Javascript",description:" Best Of JavaScript",image:"blog-post-thumb-2.jpg"),
+                new Article(id:3,title:"Learn Asp.net core mvc",description:" Best Of seller",image:"blog-post-thumb-3.jpg"),
+
+            };
+            return View(viewName: "_LatestArticles",articles);
+        }
+    }
+}
